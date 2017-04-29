8fb94c4b3db60c95a6215c8ecfdb4c59615d03bc
lichao <294645832@qq.com>
2017-04-29 14:13:18 +0800

    react-router

--- src/index.js
+++ src/index.js
@@ -1,9 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+// import React from 'react';
 import './css/style.css';
-import Index from './js/index';
 import 'antd/dist/antd.css';
-ReactDOM.render(
-  <Index />,
-  document.getElementById('root')
-);
+import './js/root';
--- src/js/root.js
+++ src/js/root.js
@@ -1,3 +1,24 @@
 /**
  * Created by lichao on 2017/4/29.
  */
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import Index from './index';
+import bodyIndex from './components/bodyIndex.js';
+import {Router, Route, hashHistory } from 'react-router';
+
+export default class Root extends Component {
+    render() {
+        return (
+            <Router history={hashHistory}>
+                <Route component={Index} path="/"/>
+                <Route component={bodyIndex} path="/a"/>
+            </Router>
+        );
+    }
+}
+
+ReactDOM.render(
+    <Root />,
+    document.getElementById('root')
+);
